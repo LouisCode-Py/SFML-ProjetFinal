a9c92b585414daa4774b926243eb3304dc7bb65b
loulo <lou2007louz@gmail.com>
2026-04-22 10:15:46 -0400

    sa marche

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "../cmake-build-debug/_deps/sfml-src/src/SFML/Window/InputImpl.hpp"
 
 int main()
 {
 	sf::RenderWindow window( sf::VideoMode( { 800, 800 } ), "SFML works!" );
-	sf::CircleShape shape( 400.f );
+	sf::CircleShape shape( 40.f );
 	shape.setFillColor( sf::Color::Green );
 
 	while ( window.isOpen() )
@@ -18,6 +19,16 @@ int main()
 		window.clear();
 		window.draw( shape );
 		window.display();
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+			shape.move({0.f,-1.f});
+		} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+			shape.move({0.f,1.f});
+		} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+			shape.move({-1.f,0.f});
+		} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+			shape.move({1.f,0.f});
+		}
 	}
 	std::cout << "Hello word!" << '\n';
 }
